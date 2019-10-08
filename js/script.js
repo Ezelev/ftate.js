5d7d912628aacd8b104d742b8c3d493548ddd4de
ezelev <iiezelev@gmail.com>
2019-10-08 16:05:55 +0300

    add styling and default usage example

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+
+	var gallery = new Obzen({
+		controlsContainer: "ctrls",
+		itemsContainer: "items",
+		theme: "default",
+	});
+
+	console.log(gallery);
+
+	var gallery2 = new Obzen({
+		controlsContainer: "ctrls2",
+		itemsContainer: "items2",
+		theme: "green"
+	});
+
+	console.log(gallery2);
+
+});
